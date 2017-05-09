91df284a9cced7fb6672692e646aca172cc486c8
oleg.spakov <oleg.spakov@uta.fi>
2017-05-09 08:59:20 +0300

    'update'

--- app.js
+++ app.js
@@ -5433,8 +5433,8 @@ if (!this.Reading) {
         _textContainer = document.querySelector( this.root );
 
         this.firstPage = [
-            //'Kiitos, etta autat meita! Lue teksti rauhassa loppuun asti. Sinulla ei ole kiire, silla tama ei ole kilpailu. Kun olet lukenut sivun loppuun, klikkaa hiirella ”Jatka”, niin paaset seuraavalle sivulle.'
-            'Tämä on aloitussivu. Lue sen teksti kokonaan alusta loppuun normaalilla lukunopeudellasi. Sivun avulla keräämme tietoa siitä, miten kauan katseesi viipyy sanoissa. Kerätyn tiedon perusteella saamme algoritmille parametrin, joka määrää, miten nopeasti sanat jatkossa tavuttuvat niitä lukiessasi.'
+            'Kiitos, etta autat meita! Lue teksti rauhassa loppuun asti. Sinulla ei ole kiire, silla tama ei ole kilpailu. Kun olet lukenut sivun loppuun, klikkaa hiirella ”Jatka”, niin paaset seuraavalle sivulle.'
+            //'Tämä on aloitussivu. Lue sen teksti kokonaan alusta loppuun normaalilla lukunopeudellasi. Sivun avulla keräämme tietoa siitä, miten kauan katseesi viipyy sanoissa. Kerätyn tiedon perusteella saamme algoritmille parametrin, joka määrää, miten nopeasti sanat jatkossa tavuttuvat niitä lukiessasi.'
         ];
 
         this.texts = [
